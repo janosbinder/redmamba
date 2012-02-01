52cc8a135004a4bbce40364eaed52f47d5965de7
janibinder <janibinder@dbe1cd76-9712-a4c8-59e4-c6e0f118c1d7>
2012-02-01 13:41:05 +0000

    createrectangles.pl: Labels are also generated for the overview figure

--- trunk/createrectanges.pl
+++ trunk/createrectanges.pl
@@ -16,19 +16,28 @@ $svg_header .= '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'."\n";
 $svg_header .= '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">'."\n";
 my $svg_tail = '</svg>';
 my $svg_body = '';
+my $text_space_x = 90;
+my $text_space_y = 25;
+
+for(my $i=0; $i<$width; $i++) {
+	my $x = $text_space_x + 25 * $i;
+	$svg_body .= '<text x="'.($x+2).'" y="15" fill="black" >'.$horizontal_labels[$i]."</text>\n" if exists $horizontal_labels[$i] and $horizontal_labels[$i] ne "-" ;
+}
 
 for(my $j=0; $j<$height; $j++) {
+	my $y = $text_space_y + 25 * $j;
+	$svg_body .= '<text x="10" y="'.($y+15).'" fill="black">'.$vertical_labels[$j].'</text>' if exists $vertical_labels[$j] and $vertical_labels[$j] ne "-";
     for(my $i=0; $i<$width; $i++) {
-	my $x = 25 * $i;
-	my $y = 25 * $j;
-
-	# draw_range function checks whether it has to draw a large rectangle at the given position and return how many of next rectange positions has been occupied:
-	my $range_length = draw_range($i, $j);
-	if($range_length) {
-	    $i+=$range_length;
-	    next;
-	}
-	$svg_body .= '<rect '.get_title($i,$j).'x="'.$x.'" y="'.$y.'" width="20" height="20" fill="#cccccc" style="stroke:black;stroke-width:2" />'."\n" unless skip_rectangle($i,$j);	
+		my $x = $text_space_x + 25 * $i;
+		
+	
+		# draw_range function checks whether it has to draw a large rectangle at the given position and return how many of next rectange positions has been occupied:
+		my $range_length = draw_range($i, $j);
+		if($range_length) {
+			$i+=$range_length;
+			next;
+		}
+		$svg_body .= '<rect '.get_title($i,$j).'x="'.$x.'" y="'.$y.'" width="20" height="20" fill="#cccccc" style="stroke:black;stroke-width:2" />'."\n" unless skip_rectangle($i,$j);	
     }
 }
 
@@ -53,8 +62,8 @@ sub draw_range {
 	    my ($r1,$r2) = split(/\-/, $range);
 	    if($r1 == $i) {
 		my $width = 20 + 25*($r2-$r1);
-		my $x = 25 * $i;
-	        my $y = 25 * $j;
+		my $x = $text_space_x + 25 * $i;
+	    my $y = $text_space_y + 25 * $j;
 		$svg_body .= '<rect '.get_title($i,$j).'x="'.$x.'" y="'.$y.'" width="'.$width.'" height="20" fill="#cccccc" style="stroke:black;stroke-width:2" />'."\n";
 		return $r2-$r1;
 	    }
