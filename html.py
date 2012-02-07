8b2d66369d481ce7c886955ccaaa1bede88d63a5
sune.frankild@gmail.com <sune.frankild@gmail.com@dbe1cd76-9712-a4c8-59e4-c6e0f118c1d7>
2012-02-07 11:59:04 +0000

    Changed layout and added protloc.py

--- html.py
+++ html.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import datetime
-import datamining
 import mamba.task
 import mamba.http
 
@@ -43,12 +42,12 @@ class xnode:
 		html = []
 		html.append(self.begin_html())
 		for node in self.nodes:
-			try:
-				s = str(node)
-				s = "\r\n".join(map(lambda a: "  " + a, s.split("\r\n")))
-				html.append(s)
-			except Exception, e:
-				html.append('<span style="color: red">%s</span>' % str(e))
+			#try:
+			s = str(node)
+			s = "\r\n".join(map(lambda a: "  " + a, s.split("\r\n")))
+			html.append(s)
+			#except Exception, e:
+			#	html.append('<span style="color: red">%s</span>' % str(e))
 		html.append(self.end_html())
 		return "\r\n".join(html).rstrip()
 		
@@ -196,6 +195,24 @@ class xsection(xdiv):
 		
 class xpage(xtag):
 	
+	class xpagetable(xtable):
+		
+		def __init__(self, title):
+			xtable.__init__(self, {"class":"page-outer-table"})
+
+			row = xrow()
+			row.add(xcell("&nbsp", {"class":"page-header-left"}))
+			row.add(xcell(xh1(title), {"class":"page-header-right"}))
+			self.tbody.add(row)
+			
+			self.sidebar = xdiv("sidebar")
+			self.content = xdiv("content")
+			
+			row = xrow()
+			row.add(xcell(self.sidebar, {"class" : "sidebar-cell"}))
+			row.add(xcell(self.content))
+			self.tbody.add(row)
+	
 	def __init__(self):
 		xtag.__init__(self, "html")
 		self.head = xhead()
@@ -211,84 +228,3 @@ class xpage(xtag):
 	
 
 
-class xdemopage(xpage):
-	
-	class xpagetable(xtable):
-		
-		def __init__(self, title):
-			xtable.__init__(self, {"class":"page-outer-table"})
-
-			row = xrow()
-			row.add(xcell("&nbsp", {"class":"page-header-left"}))
-			row.add(xcell(xh1(title), {"class":"page-header-right"}))
-			self.tbody.add(row)
-			
-			row = xrow()
-			row.add(xcell("&nbsp", {"class" : "top-separator"}))
-			row.add(xcell("&nbsp", {"class" : "top-separator"}))
-			self.tbody.add(row)
-			
-			self.sidebar = xdiv("sidebar")
-			self.content = xdiv("content")
-			
-			row = xrow()
-			row.add(xcell(self.sidebar, {"class" : "sidebar-cell"}))
-			row.add(xcell(self.content))
-			self.tbody.add(row)
-			
-			
-	
-	def __init__(self, type, id):
-		xpage.__init__(self)
-		self.head.title = "Demo page"		
-		self.page = xdemopage.xpagetable("Compendium of Disease Genes")
-		
-		wrapper = xdiv("wrapper")
-		wrapper.add(self.page)
-		self.body.add(wrapper)
-		
-		tbl = xtable({"width":"100%"})
-		tbl.addrow("&nbsp", "&nbsp")
-		tbl.addrow("Last updated:", datetime.datetime.now().strftime('%a-%d-%b-%Y %H:%M:%S %Z'))
-		tbl.addrow("Disease", "8,553")
-		tbl.addrow("Genes and proteins", "2,714")
-		self.page.sidebar.add(tbl)
-		
-		
-		sec0 = xsection("Disease gene association", '<img src="figure2.png" width="60%"></img>Using an andvanced textmining pipeline against the full body of indexed medical literatur and a ontology-derived, ontology-self-curated dictionary consisting of proteins, disease, chemicals etc. we have created the worlds first resource linking genes to diseases on a scale never seen before.')
-		sec1 = xsection("Alzheimer's disease", "A dementia that results in progressive memory loss, impaired thinking, disorientation, and changes in personality and mood starting in late middle age and leads in advanced cases to a profound decline in cognitive and physical functioning and is marked histologically by the degeneration of brain neurons especially in the cerebral cortex and by the presence of neurofibrillary tangles and plaques containing beta-amyloid.")
-		sec2 = xsection("Diabetes mellitus", "Diabetes mellitus, often simply referred to as diabetes, is a group of metabolic diseases in which a person has high blood sugar, either because the body does not produce enough insulin, or because cells do not respond to the insulin that is produced.")
-		sec3 = xsection("Shigellosis", "A primary bacterial infectious disease that results in infection located in epithelium of colon, has material basis in Shigella boydii, has material basis in Shigella dysenteriae, has material basis in Shigella flexneri, or has material basis in Shigella sonnei, which produce toxins that can attack the lining of the large intestine, causing swelling, ulcers on the intestinal wall, and bloody diarrhea.")
-		self.page.content.add(sec0)
-		self.page.content.add(sec1)
-		self.page.content.add(sec2)
-		self.page.content.add(sec3)
-		
-		
-		par = xpar()
-		par.add(xfree("This is just a test of a customized section we might have. Here is an ugly table:"))
-		tbl = xtable({"frame":"hsides", "width":"100%", "style":"background: #F7FCE4"})
-		tbl.add(xfree("<caption><em>Table 1:</em> Monthly savings.</caption>"))
-		tbl.addrow("Happy", "Go", "Lucky")
-		tbl.addrow("What can I", "come up with")
-		tbl.addrow("for this row?")
-		par.add(tbl)
-		
-		self.page.content.add(xsection("Test", par))
-		
-		self.page.content.add(datamining.xtextmining(9606, "ENSP00000335657"))
-		
-		
-		
-
-class MyPage(mamba.task.Request):
-	
-	def main(self):
-		rest = mamba.task.RestDecoder(self)
-		type = rest["type"]
-		id   = rest["identifier"]
-		
-		page = xdemopage(type, id)
-		reply = mamba.http.HTMLResponse(self, str(page))
-		reply.send()
-		
