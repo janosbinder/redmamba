5a86ada2e098fb7c5e8c183dc19a89dd73e5d6b6
janibinder <janibinder@dbe1cd76-9712-a4c8-59e4-c6e0f118c1d7>
2012-02-23 15:20:31 +0000

    Major refactoring of svg.py

--- trunk/svg.py
+++ trunk/svg.py
@@ -9,7 +9,56 @@ import mamba.task
 import mamba.http
 import pg
 
-class MockSubcell(mamba.task.Request):
+class Subcell(mamba.task.Request):
+	conn_info = ['localhost','5432','ljj','visualization']
+	table = "figures"
+	database = None
+	
+	def main(self):
+		# these variables should be passed as parameters		
+		qtype = 9606
+		qid = 'ENSP00000269305'
+		
+		rest = mamba.task.RestDecoder(self)
+		page = XPage("Localization, Localization, Localization")
+		content = page.frame.content
+		XSection(content, "Results from text mining", "Here is some fact about p53")
+		#xsvg(content, self.get_localizations(qtype, qid))
+		self.create_colored_xsvg(content, qtype, qid)
+		reply = mamba.http.HTMLResponse(self, page.tohtml())
+		reply.send()
+		
+	def db_connect(self):
+		self.database = pg.connect(host=self.conn_info[0], port=int(self.conn_info[1]), user=self.conn_info[2], passwd='', dbname=self.conn_info[3])		
+		
+	def db_close(self):
+		self.database.close()
+	
+	def create_colored_xsvg(self, parent, qtype, qid):
+		compartment_color_map = {}
+		self.db_connect()
+		figure = ""
+		q  = self.database.query("SELECT * FROM colors WHERE id = '%s' AND type = %i;" % (pg.escape_string(str(qid)), qtype)).getresult()
+		#record[0] = type ,record[1] = id ,record[2] = figure , record[3] = label , record[4] = color
+		if len(q) == 0:
+			raise Exception, "Error: No record exists for %s,%s" % (str(qtype), str(qid))		
+		for record in q:
+			compartment_color_map[record[3]] = record[4]
+			figure = record[2]
+		
+		svg = xsvg(parent, self.get_figure(figure))
+		svg.recolor(compartment_color_map)
+		self.db_close()
+		return svg
+		
+	def get_figure(self, figure):
+		q  = self.database.query("SELECT svg FROM figures WHERE figure = '%s';" % (pg.escape_string(str(figure)))).getresult()
+		if len(q) != 1:
+			raise Exception, "Error: No figure called %s from table %s has been returned" % figure, table
+		svg = q[0][0];
+		return svg
+
+class MockSubcell(Subcell):
 	conn_info = ['localhost','5432','ljj','visualization']
 	table = "figures"
 	
@@ -20,29 +69,33 @@ class MockSubcell(mamba.task.Request):
 
 		evidence = XGroup(page.frame.content, "Overview - Evidence channels")
 		XSection(evidence.body, "Results from different subcellular localization methods", "Press refresh if you are not satisfied with the results. Then the monkeys will be shocked in the box and they will paint the squares with different green")
-		xsvg(evidence.body, svg_colorer.get_figure_from_file("www/figures/overview_figure.svg"), self.get_mock_different_methods(9606, 'fake'))
+		evidence_svg = xsvg.get_svg_from_file(evidence.body, "www/figures/overview_figure.svg")
+		evidence_svg.recolor(self.get_mock_different_methods(9606, 'fake'))		
 		
 		kingdoms = XGroup(page.frame.content, "Cell types - Kingdoms of life")
 
+		self.db_connect()
 		XSection(kingdoms.body, "Results from text mining - plant", "Press refresh if you are not satisfied with the results. Then the boxes will be colored here using some random noise")
-		xsvg(kingdoms.body, svg_colorer.get_figure(self.conn_info,self.table,'cell_plants'), self.get_mock_subcell_loc(9606, 'fake'))
+		xsvg(kingdoms.body, self.get_figure('cell_plants')).recolor(self.get_mock_subcell_loc(9606, 'fake'))
 
 		XSection(kingdoms.body, "Results from text mining - animal", "Press refresh if you are not satisfied with the results. Then the boxes will be colored here using some random noise")
-		xsvg(kingdoms.body, svg_colorer.get_figure(self.conn_info,self.table,'cell_animals'), self.get_mock_subcell_loc(9606, 'fake'))
+		xsvg2 = xsvg(kingdoms.body, self.get_figure('cell_animals')).recolor(self.get_mock_subcell_loc(9606, 'fake'))
 		
 		XSection(kingdoms.body, "Results from text mining - fungus", "Press refresh if you are not satisfied with the results. Then the boxes will be colored here using some random noise")
-		xsvg(kingdoms.body, svg_colorer.get_figure(self.conn_info,self.table,'cell_fungi'), self.get_mock_subcell_loc(9606, 'fake'))
+		xsvg(kingdoms.body, self.get_figure('cell_fungi')).recolor(self.get_mock_subcell_loc(9606, 'fake'))
 
+		self.db_close()
+		
 		reply = mamba.http.HTMLResponse(self, page.tohtml())
 		reply.send()
 		
 	def get_mock_subcell_loc(self, type, id):
 		compartments = ['nu','cy','cs','pe','ly','er','go','pm','en','va','ex','mi']
-		compartment_score_map = {}
+		compartment_color_map = {}
 		random.seed()
 		for compartment in compartments:
-			compartment_score_map[compartment] = MockSubcell.define_color(random.random())
-		return compartment_score_map
+			compartment_color_map[compartment] = MockSubcell.define_color(random.random())		
+		return compartment_color_map
 	
 	def get_mock_different_methods(self, type, id):
 		compartments = ['nu','cy','cs','pe','ly','er','go','pm','en','va','ex','mi']
@@ -59,101 +112,55 @@ class MockSubcell(mamba.task.Request):
 	def define_color(w):
 		return "#%02x%02x%02x" % (255*(1-w), 255*(1-0.5*w), 255*(1-w))
 
-class Subcell(mamba.task.Request):
-	#TODO, make database connection work, make sure that it can accept passed parameters
-	#create some normal HTML page
-	#p53 ensembl ENSP00000269305
-	
-	conn_string = ['localhost','5432','ljj','knowledge']	
-	go_location_dict = {'GO:0005576':'ex', 'GO:0005634':'nu', 'GO:0005739':'mi', 'GO:0005764':'ly', 'GO:0005768':'en', 'GO:0005773':'va', 'GO:0005777':'pe', 'GO:0005783':'er', 'GO:0005794':'go', 'GO:0005829':'cy', 'GO:0005856':'cs', 'GO:0005886':'pm', 'GO:0009507':'ch'}
-	
-	def main(self):
-		# these variables should be passed as parameters		
-		type = 9606
-		id = 'ENSP00000269305'
-		
-		rest = mamba.task.RestDecoder(self)
-		page = XPage("Localization, Localization, Localization")
-		content = page.frame.content
-		XSection(content, "Results from text mining", "Here is some fact about p53")
-		xsvg(content, "www/figures/subcell.svg", self.get_localizations(type, id))
-		reply = mamba.http.HTMLResponse(self, page.tohtml())
-		reply.send()
-		
-	def get_localizations(self, type, id):
-		compartment_score_map = {}
-		for go in Subcell.go_location_dict:
-			compartment_score_map[Subcell.go_location_dict[go]] = 0.0
-		conn = pg.connect(host = self.conn_string[0], port = int(self.conn_string[1]), user = self.conn_string[2], passwd = '', dbname = self.conn_string[3])
-		query = conn.query("SELECT * FROM knowledge WHERE type1 = %s AND id1 = '%s'" % (pg.escape_string(str(type)), pg.escape_string(id)))
-		for result in query.dictresult():
-			if (result['id2'] in self.go_location_dict):
-				weight = float(result['score'])/5
-				compartment_score_map[self.go_location_dict[result['id2']]] = weight
-		compartment_score_map = {'va': 0.0, 'ch': 0.0, 'mi': 0.0, 'cs': 0.0, 'cy': 0.4, 'en': 0.1, 'ex': 0.0, 'pe': 0.0, 'go': 0.3, 'ly': 0.0, 'er': 0.2, 'nu': 0.5, 'pm': 0.0}
-		conn.close()
-		return compartment_score_map
-		
-		
 
 class xsvg(XNode):
+	__svg = ''	
 	
-	def __init__(self, parent, input_string, compartment_color_map):
+	def __init__(self, parent, svg_as_string):
 		XNode.__init__(self, parent)
-		self.input_string = input_string
-		self.compartment_color_map = compartment_color_map
+		self.__svg = svg_as_string
 		
 	def begin_html(self):
-		return svg_colorer.create_colored_html(self.input_string, self.compartment_color_map)
-
-class svg_colorer:
-	
-	@staticmethod
-	def get_figure(conn_info, table, figure):
-		conn_info = ['localhost','5432','ljj','visualization']
-		table = "figures"
-		#figure = "cell_animals"
-		database = pg.connect(host=conn_info[0], port=int(conn_info[1]), user=conn_info[2], passwd='', dbname=conn_info[3])
-		q  = database.query("SELECT svg FROM %s WHERE figure = '%s';" % (pg.escape_string(str(table)),pg.escape_string(str(figure)))).getresult()
-		if len(q) != 1:
-			raise Exception, "Error: No figure called %s from table %s has been returned" % figure, table
-		return q[0][0]
+		return self.__svg
 		
-	@staticmethod
-	def concatenate_list(mylist, prefix, suffix, separator):
-		str_list = [];
-		for s in mylist:
-			str_list.append(prefix)
-			str_list.append(s)
-			str_list.append(suffix)
-			str_list.append(separator)
-		str_list.pop();
-		return string.join(str_list,'')
-
-	@staticmethod
-	def create_colored_html(input_svg, compartment_color_map):	
-		buf = [];
-		exp = svg_colorer.concatenate_list(compartment_color_map.keys(),'<.* title="','".*>','|')
-		for line in input_svg.split("\n"):
+	def replace_content(self, new_content):
+		self.svg = new_content
+	
+	def get_content(self):
+		return self.__svg
+	
+	def recolor(self, label_color_map):
+		buf = []
+		l = []
+		
+		for key in label_color_map.keys():
+			l.append('<.* title="')
+			l.append(key)
+			l.append('".*>')
+			l.append('|')
+		l.pop()
+		exp = string.join(l,'')
+		
+		for line in self.__svg.split("\n"):
 			if re.search("^ *<[?!]", line): #ignore XML header
 				continue
-			match = re.search(exp, line)
-			if match:
-				for key, value in compartment_color_map.iteritems():
-					match2 = re.search('title="'+key+'"',line)
-					if match2:
+			if re.search(exp, line):
+				for key, value in label_color_map.iteritems():
+					if re.search('title="'+key+'"',line):
 						buf.append(re.sub('(?<=fill:|ill=")#.{6}', '%s' % value, line))
 			else:
 				buf.append(line)
-		return string.join(buf,"\n")
+		self.__svg = string.join(buf,"\n")
+		return
 		
 	@staticmethod
-	def get_figure_from_file(filename):
-		f = open(filename, 'r')
+	def get_svg_from_file(parent, svg_as_file):
+		f = open(svg_as_file, 'r')
 		buf = []
 		for line in f:
+			if re.search("^ *<[?!]", line): #ignore XML header
+				continue
 			line = line[:-1]
 			buf.append(line)
 		f.close()
-		return string.join(buf,"\n")
-
+		return xsvg(parent, string.join(buf,"\n"))		
